83f4c5497f690dcda3e8919a994f588a4a2b3af3
fabiofilz <fabiofilz@gmail.com>
2022-09-18 12:52:40 +0100

    Dynamic query using JPQL with optional parameters

--- src/main/java/com/fabiofilz/restaurantapi/infrastructure/repository/RestaurantRepositoryImpl.java
+++ src/main/java/com/fabiofilz/restaurantapi/infrastructure/repository/RestaurantRepositoryImpl.java
@@ -3,10 +3,13 @@ package com.fabiofilz.restaurantapi.infrastructure.repository;
 import com.fabiofilz.restaurantapi.domain.model.Restaurant;
 import com.fabiofilz.restaurantapi.domain.repository.RestaurantRepositoryQueries;
 import org.springframework.stereotype.Repository;
+import org.springframework.util.StringUtils;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
 import java.math.BigDecimal;
+import java.util.HashMap;
 import java.util.List;
 
 @Repository
@@ -18,14 +21,32 @@ public class RestaurantRepositoryImpl implements RestaurantRepositoryQueries {
   @Override
   public List<Restaurant> findImplQueries(String name, BigDecimal deliveryFeeFrom, BigDecimal deliveryFeeTo){
 
-    var jpql = "from Restaurant where name like :name "
-              + "and deliveryFee between :deliveryFeeFrom and :deliveryFeeTo";
+    var jpql = new StringBuilder();
+    jpql.append("from Restaurant where 0 = 0 ");
 
-    return entityManager.createQuery(jpql, Restaurant.class)
-        .setParameter("name", "%" + name + "%")
-        .setParameter("deliveryFeeFrom", deliveryFeeFrom)
-        .setParameter("deliveryFeeTo", deliveryFeeTo)
-        .getResultList();
+    var params = new HashMap<String, Object>();
+
+    if (StringUtils.hasLength(name)) {
+      jpql.append("and name like :name ");
+      params.put("name", "%" + name + "%");
+    }
+
+    if (deliveryFeeFrom != null) {
+      jpql.append("and deliveryFee >= :deliveryFeeFrom ");
+      params.put("deliveryFeeFrom", deliveryFeeFrom);
+    }
+
+    if (deliveryFeeTo != null) {
+      jpql.append("and deliveryFee <= :deliveryFeeTo ");
+      params.put("deliveryFeeTo", deliveryFeeTo);
+    }
+
+    TypedQuery<Restaurant> query = entityManager
+        .createQuery(jpql.toString(), Restaurant.class);
+
+    params.forEach((key, valor) -> query.setParameter(key, valor));
+
+    return query.getResultList();
 
   }
 
